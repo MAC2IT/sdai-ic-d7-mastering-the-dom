f9802966793b5e5f38ccdd4ff2facc863b498b07
MAC2IT <sourcefullresources@gmail.com>
2024-11-14 04:38:25 +0000

    added cat removal

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Write your JavaScipt code below this line. 
+const maintitle = document.getElementById("main-title")
+maintitle.textContent = "Welcome to the World of Cats"
+
+const description = document.getElementById('description')
+description.textContent = "Find your Purrfect Companion"
+
+const cat1Button = document.getElementById('cat-1-button')
+cat1Button.addEventListener('click', () => {
+    const cat = document.getElementById('cat-1')
+    cat.remove()
+    alert('Congrats on adopting your new Whiskers!')
+})
+
+const cat2Button = document.getElementById('cat-2-button')
+cat1Button.addEventListener('click', () => {
+    const cat = document.getElementById('cat-2')
+    cat.remove()
+    alert('Congrats on adopting your new Daisy!')
+
+})
